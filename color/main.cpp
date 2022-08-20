270e2c91e84d92d5fd18efc6bfd74fb55d4e0512
Ubuntu <iwilkey@mail.bradley.edu>
2022-08-20 16:47:55 +0000

    + problems

--- color/main.cpp
+++ color/main.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 int main(void) {
-
     int s, c, k; cin >> s >> c >> k;
     vector<int> colors;
     for(int i = 0; i < s; i++) {
@@ -13,23 +12,21 @@ int main(void) {
         cin >> in;
         colors.push_back(in);
     }
-
     sort(colors.begin(), colors.end());
-
     int w = 0;
     while(true) {
         int ind = 0;
-        for(int i = 1; i < s; i++) {
-            if(ind + 1 > c) break;
-            if(abs(colors[i] - colors[ind]) <= k) {
+        for(int i = 1; i < colors.size(); i++) {
+            if(ind + 1 >= c) break;
+            if(abs(colors[i] - colors[0]) <= k) {
                 ind = i;
             } else break;
         }
         w++;
-        colors.erase(colors.begin() + ind);
+        for(int i = 0; i <= ind; i++)
+            colors.erase(colors.begin());
         if(colors.size() == 0) break;
     }
     cout << w << endl;
-
     return 0;
 }
