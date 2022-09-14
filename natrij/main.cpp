24935a14b6dbd188d8968d4c0d34f3e208ed26a7
Ian Wilkey <ian.wilkey@cat.com>
2022-09-13 21:30:40 -0500

    + problems

---
+++ natrij/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+    string time1, time2;
+    cin >> time1 >> time2;
+
+    for(int i = 0; i < 8; i++) {
+        if(i == 2 || i == 5) continue;
+        
+    }
+
+    return 0;
+}
